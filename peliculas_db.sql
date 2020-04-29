f6544b04b34e6a1b037b7b8fa6deb76794c16600
PedroMatadamas <32680379+PedroMatadamas@users.noreply.github.com>
2020-04-28 22:49:00 -0500

    Update peliculas_db.sql

--- peliculas_db.sql
+++ peliculas_db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE peliculas_db;
 Create database if not exists peliculas_db;
 use peliculas_db;
 
@@ -18,35 +19,19 @@ create table if not exists actores(
     a_apellidoPat varchar(25) not null,
     a_apellidoMat varchar(25),
     a_nacionalidad varchar(25) not null,
-    a_fnaciento DATE,
+    a_fnacimiento DATE,
     primary key(id_actor)
 )engine = InnoDB;
 
 create table if not exists peliculas(
 		id_pelicula int not null auto_increment,
-    	p_titulo varchar(45) not null,
+    	p_titulo varchar(25) not null,
     	p_idioma varchar(20) not null,
     	p_subtitulos varchar(40),
     	p_año varchar(5),
     	primary key(id_pelicula)
 )engine = InnoDB;
 
-
-create table if not exists dirpeliculas(
-		dp_id_director int not null,
-		dp_id_pelicula int not null,
-		primary key(dp_id_director, dp_id_pelicula),
-		constraint fkdp_id_director 
-        foreign key(dp_id_director)
-		references directores(id_director)
-        ON DELETE CASCADE
-        on update cascade,
-		constraint fkdp_id_pelicula 
-        foreign key(dp_id_pelicula)
-		references peliculas(id_pelicula)
-        ON DELETE CASCADE
-        on update cascade
-)engine = InnoDB;
 
 create table if not exists actorpeliculas(
 		ap_id_actor int not null,
@@ -67,13 +52,20 @@ create table if not exists actorpeliculas(
 create table if not exists detalles_peliculas(
 		id_dp int not null,
 		dp_id_pelicula int not null,
-        sinopsis varchar(100) not null,
+        dp_id_director int ,
+        descripcion varchar(100) not null,
 		primary key(id_dp, dp_id_pelicula),
 		constraint fk_dp_id_pelicula 
         foreign key(dp_id_pelicula)
 		references peliculas(id_pelicula)
         ON DELETE CASCADE
+        on update cascade,
+		constraint fkdp_id_director 
+        foreign key(dp_id_director)
+		references directores(id_director)
+		ON DELETE SET NULL
         on update cascade
+        
 )engine = InnoDB;
 
 create table if not exists genero(
@@ -89,6 +81,7 @@ create table if not exists genpelicula(
 		constraint fkgp_id_genero 
         foreign key(gp_id_genero)
 		references genero(id_genero)
+        ON DELETE CASCADE
         on update cascade,
 		constraint fkgp_id_pelicula 
         foreign key(gp_id_pelicula)
@@ -96,3 +89,4 @@ create table if not exists genpelicula(
         ON DELETE CASCADE
         on update cascade
 )engine = InnoDB;
+
